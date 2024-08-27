574c5f3f8eaeaaa18b46aa11139bcaf9dd86c6d1
kirillsats <153904960+kirillsats@users.noreply.github.com>
2024-08-27 16:27:51 +0300

    Add files via upload

---
+++ funktsioonid.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARpv23_KirillSats
+{
+    internal class funktsioonid
+    {
+        public static void Tere(string nimi)
+        {
+            Console.WriteLine("Tere kallis {0}", nimi);
+        }
+        public static int Liitmine(int arv1, int arv2)
+        {
+            return arv1 + arv2;
+        }
+        // Loo Arvuta() funktsioon, mis sõltub 3 parameetrist: tehe, arv1, arv2. Kasuta if konstruktsioon. Tulemus kuva ekraanile.
+
+
+        public static int Arvuta(char tehe, int arv1, int arv2)
+        {
+            int tulemus = 0;
+
+            if (tehe == '+')
+            {
+                tulemus = arv1 + arv2;
+            }
+            else if (tehe == '-')
+            {
+                tulemus = arv1 - arv2;
+            }
+            else
+            {
+                Console.WriteLine("Viga! Tundmatu tehe: " + tehe);
+                return 0; 
+            }
+
+            return tulemus; 
+        }
+        //Pinginaabrid #1
+        public static string Naaber(string nimi1, string nimi2)
+        {
+            return "Naaber on " + nimi1 + " ja " + nimi2;
+        }
+
+        //Temperatuur #2
+        public class Funktsioonid
+        {
+            public string Temp(double temperatuur)
+            {
+                if (temperatuur > 18)
+                {
+                    return "Temperatuur on üle kaheksateistkümne kraadi";
+                }
+                else if (temperatuur == 18)
+                {
+                
+                        return "Hea temperatuur";
+                }
+                else
+                {
+                    return "Temperatuur on alla kaheksateistkümne kraadi";
+                }
+            }
+        }
+        //Soodushind #3
+        public static double alghind(double praeguneHind, double soodustusProtsent)
+        {
+            // Находим стартовую цену, зная текущую цену и процент скидки
+            double alghind = praeguneHind / (1 - soodustusProtsent / 100);
+            return alghind;
+        }
+
+        //Pikkus #4
+        public string Rost(double kasv)
+        {
+            if (kasv < 160)
+            {
+                return "Pikkus on madal.";
+            }
+            else if (kasv <= 180)
+            {
+                return "Pikkus on keskmine.";
+            }
+            else
+            {
+                return "Pikkus on kõrge.";
+            }
+        }
+
+    }
+}
---
+++ mainclass.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static TARpv23_KirillSats.funktsioonid;
+
+namespace TARpv23_KirillSatsˇ               //ПРЕЖДЕ ЧЕМ ЗАПУСТИТЬ ОДНО НУЖНО ЗАКОММЕНТИТЬ ОСТАЛЬНОЕ
+{
+    public class mainclass
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Hello World!");
+            string nimetus = "Python";
+            Console.WriteLine("Hello {0}", nimetus);
+            funktsioonid.Tere(nimetus);
+            Console.Write("Sisesta esimine arv: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Sisesta teine arv: ");
+            int b = int.Parse(Console.ReadLine());
+            int vastus = funktsioonid.Liitmine(a, b);
+            Console.WriteLine("Teie vastus on: {0}", vastus);
+            double arv = 5.123456;
+            vastus = funktsioonid.Liitmine(a, (int)arv);
+            Console.WriteLine("Teie vastus on: {0}", vastus);
+            char taht = 'A';
+            if (vastus == 0)
+            {
+                Console.WriteLine(vastus);
+            }
+            else
+            {
+                Console.WriteLine(vastus);
+            }
+            
+
+            //Arvutama
+            Console.Write("Sisesta sümbol: ");
+            char sümbol = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sisesta esimene arv:");
+            int o = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sisesta teine arv:");
+            int p = int.Parse(Console.ReadLine());
+
+            int tulemus = funktsioonid.Arvuta(sümbol, o, p);
+
+            Console.WriteLine($"Teie vastus on: {tulemus}");
+            
+
+
+            //Naaber #1
+            Console.Write("Sisestage oma nime: ");
+            string nimi1 = Console.ReadLine();       // Считываем введённое имя и сохраняем его в переменную 'nimi'
+            Console.WriteLine("Sisestage lähima inimese nimi: ");
+            string nimi2 = Console.ReadLine();
+            string naabrid = funktsioonid.Naaber(nimi1, nimi2);
+            Console.WriteLine(naabrid);
+
+
+            //Temperatuur #2
+
+            Funktsioonid funktsioonid = new Funktsioonid();
+
+            
+            Console.Write("Sisestage temperatuur: ");
+            double temperatuur = double.Parse(Console.ReadLine());
+
+            string tulemus = funktsioonid.Temp(temperatuur);
+
+            Console.WriteLine(tulemus);
+
+            //Soodushind #3
+
+            double ArvutaHind(double praeguneHind, double soodustusProtsent)
+            {
+                return praeguneHind / (1 - soodustusProtsent / 100);
+            }
+
+            Console.Write("Sisestage praegune hind: ");
+            double praeguneHind = double.Parse(Console.ReadLine());
+
+            Console.Write("Sisestage soodustus protsentides: ");
+            double soodustusProtsent = double.Parse(Console.ReadLine());
+
+            double startHind = funktsioonid.alghind(praeguneHind, soodustusProtsent);
+            Console.WriteLine("Start hind, enne soodustust, on: " + startHind);
+
+            //Pikkus #4
+            
+            Console.Write("Sisestage oma pikkus (cm): ");
+            double kasv = double.Parse(Console.ReadLine());
+
+                
+            string tulemus = funktsioonid.Rost(kasv);
+
+            Console.WriteLine(tulemus);
+            
+
+
+
+
+
+        }
+
+    }
+}
