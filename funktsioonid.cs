4d490aa7e6ab88d630094c791aac3379af202ddd
kirillsats <153904960+kirillsats@users.noreply.github.com>
2024-08-28 12:42:30 +0300

    funktsioonid.cs

--- funktsioonid.cs
+++ funktsioonid.cs
@@ -1,97 +1,121 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using TARpv23_KirillSats;
+using static TARpv23_KirillSats.funktsioonid;
 
-namespace TARpv23_KirillSats
+namespace TARpv23_KirillSats   //ПРЕЖДЕ ЧЕМ ЗАПУСТИТЬ ОДНО, НУЖНО ЗАКОММЕНТИТЬ ОСТАЛЬНОЕ
 {
-    internal class funktsioonid
+    public class mainclass
     {
-        public static void Tere(string nimi)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Tere kallis {0}", nimi);
-        }
-        public static int Liitmine(int arv1, int arv2)
-        {
-            return arv1 + arv2;
-        }
-        // Loo Arvuta() funktsioon, mis sõltub 3 parameetrist: tehe, arv1, arv2. Kasuta if konstruktsioon. Tulemus kuva ekraanile.
-
-
-        public static int Arvuta(char tehe, int arv1, int arv2)
-        {
-            int tulemus = 0;
-
-            if (tehe == '+')
+            Console.OutputEncoding = Encoding.UTF8;
+            /*Console.WriteLine("Hello World!");
+            string nimetus = "Python";
+            Console.WriteLine("Hello {0}", nimetus);
+            funktsioonid.Tere(nimetus);
+            Console.Write("Sisesta esimine arv: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Sisesta teine arv: ");
+            int b = int.Parse(Console.ReadLine());
+            int vastus = funktsioonid.Liitmine(a, b);
+            Console.WriteLine("Teie vastus on: {0}", vastus);
+            double arv = 5.123456;
+            vastus = funktsioonid.Liitmine(a, (int)arv);
+            Console.WriteLine("Teie vastus on: {0}", vastus);
+            char taht = 'A';
+            if (vastus == 0)
             {
-                tulemus = arv1 + arv2;
-            }
-            else if (tehe == '-')
-            {
-                tulemus = arv1 - arv2;
+                Console.WriteLine(vastus);
             }
             else
             {
-                Console.WriteLine("Viga! Tundmatu tehe: " + tehe);
-                return 0; 
-            }
+                Console.WriteLine(vastus);
+            }*/
 
-            return tulemus; 
-        }
-        //Pinginaabrid #1
-        public static string Naaber(string nimi1, string nimi2)
-        {
-            return "Naaber on " + nimi1 + " ja " + nimi2;
-        }
 
-        //Temperatuur #2
-        public class Funktsioonid
-        {
-            public string Temp(double temperatuur)
+            //Arvutama
+            /* Console.Write("Sisesta sümbol: ");
+             char sümbol = char.Parse(Console.ReadLine());
+
+             Console.WriteLine("Sisesta esimene arv:");
+             int o = int.Parse(Console.ReadLine());
+
+             Console.WriteLine("Sisesta teine arv:");
+             int p = int.Parse(Console.ReadLine());
+
+             int tulemus = funktsioonid.Arvuta(sümbol, o, p);
+
+             Console.WriteLine($"Teie vastus on: {tulemus}");*/
+
+
+
+            //Naaber #1
+            /*Console.Write("Sisestage oma nime: ");
+            string nimi1 = Console.ReadLine();       // Считываем введённое имя и сохраняем его в переменную 'nimi'
+            Console.WriteLine("Sisestage lähima inimese nimi: ");
+            string nimi2 = Console.ReadLine();
+            string naabrid = funktsioonid.Naaber(nimi1, nimi2);
+            Console.WriteLine(naabrid);*/
+
+
+            //Temperatuur #2
+
+            /*Funktsioonid funktsioonid = new Funktsioonid();
+
+
+            Console.Write("Sisestage temperatuur: ");
+            double temperatuur = double.Parse(Console.ReadLine());
+
+            string tulemus = funktsioonid.Temp(temperatuur);
+
+            Console.WriteLine(tulemus);*/
+
+            //Soodushind #3
+
+            /*double ArvutaHind(double praeguneHind, double soodustusProtsent)
             {
-                if (temperatuur > 18)
-                {
-                    return "Temperatuur on üle kaheksateistkümne kraadi";
-                }
-                else if (temperatuur == 18)
-                {
-                
-                        return "Hea temperatuur";
-                }
-                else
-                {
-                    return "Temperatuur on alla kaheksateistkümne kraadi";
-                }
+                return praeguneHind / (1 - soodustusProtsent / 100);
             }
-        }
-        //Soodushind #3
-        public static double alghind(double praeguneHind, double soodustusProtsent)
-        {
-            // Находим стартовую цену, зная текущую цену и процент скидки
-            double alghind = praeguneHind / (1 - soodustusProtsent / 100);
-            return alghind;
-        }
 
-        //Pikkus #4
-        public static string Pikkuse_analuus(double kasv)
-        {
-            string analuus;
-        if (kasv < 1.6)
-        {
-            analuus = "Lühike kasv";
-        
-        }
-        else if (kasv < 1.8)
-        {
-        analuus = "Pikkus on keskmine.";
-        }
-        else
-        {
-        analuus = "Pikkus on kõrge.";
-        }
-        return analuus;
+            Console.Write("Sisestage praegune hind: ");
+            double praeguneHind = double.Parse(Console.ReadLine());
+
+            Console.Write("Sisestage soodustus protsentides: ");
+            double soodustusProtsent = double.Parse(Console.ReadLine());
+
+            double startHind = funktsioonid.alghind(praeguneHind, soodustusProtsent);
+            Console.WriteLine("Start hind, enne soodustust, on: " + startHind);*/
+
+            //Pikkus #4
+
+            /*Console.Write("Sisestage oma pikkus (m): ");
+            double kasv = double.Parse(Console.ReadLine());
+
+
+            string tulemus = funktsioonid.Pikkuse_analuus(kasv);
+
+            Console.WriteLine(tulemus);*/
+
+            //Päeva number-> päeva nimetus
+
+            for (int i = 0; 1 < 7; i++)   //цикл, больше 1 и меньше 7. I++ увеличивает на 1. Как только доходит до 7 выходит из цикла
+            {
+                Random random = new Random();
+                int paev_nr = random.Next(-4, 30);
+                string paeva_nimetus = funktsioonid.Paevad(paev_nr);
+                Console.WriteLine(paeva_nimetus);
+            }
+
+
+
+
+
+
+
         }
 
     }
